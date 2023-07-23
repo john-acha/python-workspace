609dc3fe90f242ea45006ad1e4f7178918608334
john-acha <john_acha@live.com>
2023-07-22 20:11:53 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+first_name = 'john'
+last_name = 'acha'
+full_name = f'{first_name} {last_name}'
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+message = "Welcome to Python World!"
+print(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
